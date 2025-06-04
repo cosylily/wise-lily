c5ab35163165cc92b8c3baebaf1ef74d11b0db9d
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-04 16:16:12 +0100

    downloaded axios
    installed axiom multiple times and FINALLY figure out that i have installed the wrong package this whole time T_T

--- src/Dictionaryform.js
+++ src/Dictionaryform.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 export default function DictionaryForm() {
   let [word, setword] = useState({});
+  let apiKey = "3caeb6a7a0144t0bff6oc0e38972db67";
 
   function wordSearch(event) {
     event.preventDefault();
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
     console.log(`Searching for ${word}`);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data);
   }
   function handlingWord(event) {
     console.log(event.target.value);
