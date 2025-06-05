b2201b7b0a16fb4d13131bb05485e70685306cd1
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-05 20:21:36 +0100

    structure for definition

--- src/Dictionarycall.js
+++ src/Dictionarycall.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "./App.css";
 
 export default function Wordcalling(props) {
   if (props.need.word) {
-    return <div>Yo!</div>;
+    return (
+      <div className="border rounded p-1 m-2">
+        <div className="firstSec m-3">
+          <div className="theword">{props.need.word}</div>
+          <div className="phonetics">🔈{props.need.phonetics}</div>
+        </div>
+        <div className="secondSec p-3 mt-3">
+          <div className="definition">Definition: {props.need.definition}</div>
+          <div className="synonym">Similar word: {props.need.synonym}</div>
+        </div>
+      </div>
+    );
   } else {
     return null;
   }
