6ce382163900a8bc2b7b49805624cf9ca0860477
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-05 15:06:30 +0100

    finish with css

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import DictionaryForm from "./Dictionaryform";
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App p-2 container">
       <div className="title">
         <h1 className="text-center m-3 p-2">Dictionary</h1>
         <h3 className="m-3 p-2">Understand English words without any hassle</h3>
       </div>
-
-      <DictionaryForm />
-      <footer className="m-3 text-center">
-        This dictionary app is created by Hikari. Access the code through{" "}
-        <a href="https://github.com/cosylily/wise-lily.git">GitHub</a> and{" "}
-        <a href="https://incredible-froyo-08762a.netlify.app/">Netfily</a>.
-      </footer>
+      <div className="main-body p-2 rounded">
+        <DictionaryForm />
+        <footer className="m-3 text-center">
+          This dictionary app is created by Hikari. Access the code through{" "}
+          <a href="https://github.com/cosylily/wise-lily.git">GitHub</a> and{" "}
+          <a href="https://incredible-froyo-08762a.netlify.app/">Netfily</a>.
+        </footer>
+      </div>
     </div>
   );
 }
