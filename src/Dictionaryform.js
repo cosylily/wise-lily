9d71d70c86d73984a67558ac9c28d099023d2043
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-05 17:37:00 +0100

    list out needed data

--- src/Dictionaryform.js
+++ src/Dictionaryform.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Wordcalling from "./Dictionarycall";
 import "./App.css";
 
 export default function DictionaryForm() {
   let [word, setword] = useState({});
+  let [wordInfo, setwordInfo] = useState({});
   let apiKey = "3caeb6a7a0144t0bff6oc0e38972db67";
 
   function wordSearch(event) {
@@ -14,27 +16,37 @@ export default function DictionaryForm() {
   }
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response);
+    setwordInfo({
+      rawdata: response.data,
+      word: response.data.word,
+      definition: response.data.meanings[0].definition,
+      phonetics: response.data.phonetic,
+      synonym: response.data.meanings[0].synonyms,
+    });
   }
   function handlingWord(event) {
     console.log(event.target.value);
     setword(event.target.value);
   }
   return (
-    <div className="form">
-      <form onSubmit={wordSearch} className="">
-        <input
-          type="search"
-          onChange={handlingWord}
-          placeholder="Look for the word..."
-          className="mt-3 mb-3 p-2 border border-end-0 rounded rounded-end-0 searchBox"
-        />
-        <input
-          type="submit"
-          value="🔍"
-          className="border border-start-0 rounded rounded-start-0 p-2 mt-3 mb-3 submitButton"
-        />
-      </form>
+    <div className="definiton-body">
+      <div className="form">
+        <form onSubmit={wordSearch} className="">
+          <input
+            type="search"
+            onChange={handlingWord}
+            placeholder="Look for the word..."
+            className="mt-3 mb-3 p-2 border border-end-0 rounded rounded-end-0 searchBox"
+          />
+          <input
+            type="submit"
+            value="🔍"
+            className="border border-start-0 rounded rounded-start-0 p-2 mt-3 mb-3 submitButton"
+          />
+        </form>
+      </div>
+      <Wordcalling need={wordInfo} />
     </div>
   );
 }
