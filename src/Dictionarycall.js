3ee28e38bb75ba07656b608000ddc32f34ffda13
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-06 15:54:22 +0100

    mapping of definitons

--- src/Dictionarycall.js
+++ src/Dictionarycall.js
@@ -6,11 +6,11 @@ export default function Wordcalling(props) {
   if (props.need.word) {
     return (
       <div className="border rounded p-1 m-2">
-        <div className="firstSec m-3">
+        <div className="firstSec ms-3 mt-3">
           <div className="theword">{props.need.word}</div>
           <div className="phonetics">🔈{props.need.phonetic}</div>
         </div>
-        <div className="secondSec p-3 mt-3">
+        <div className="secondSec p-3">
           {props.need.meanings.map(function (meaning, index) {
             return (
               <div key={index}>
--- src/Dictionarymeaning.js
+++ src/Dictionarymeaning.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Meaning() {
-  return <div></div>;
+export default function Meaning(props) {
+  return (
+    <div className="definitionSec">
+      <div className="titledefinition mb-2">{props.meaning.partOfSpeech}:</div>
+      <div className="definition p-2">{props.meaning.definition}</div>
+      <div className="example p-2 mb-2">example: {props.meaning.example}</div>
+    </div>
+  );
 }
