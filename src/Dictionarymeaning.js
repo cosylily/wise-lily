c4d2f61edd413c1744c879c122f4a591ef69f556
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-11 10:07:20 +0100

    created a component for synonyms

--- src/Dictionarymeaning.js
+++ src/Dictionarymeaning.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./meaning.css";
+import Synonyms from "./Dictionarysynonyms";
 
 export default function Meaning(props) {
   return (
     <div className="definitionSec">
-      <div className="titledefinition mb-2">{props.meaning.partOfSpeech}:</div>
+      <div className="titledefinition mb-2">{props.meaning.partOfSpeech}</div>
       <div className="definition p-2">{props.meaning.definition}</div>
       <div className="example p-2">Example: {props.meaning.example}</div>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
