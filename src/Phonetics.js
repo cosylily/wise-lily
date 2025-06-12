52dfc7aa8c6ca8b820f59279a42915f8dccdea5d
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-12 16:00:09 +0100

    fixed on how phonetics audio is displayed

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  if (props.phonetics) {
-    return <div>Yo</div>;
+  if (props.phonetics.audio && props.phonetics.text) {
+    return (
+      <div>
+        <a href={props.phonetics.audio} target="_blank">
+          🔊
+        </a>
+        {props.phonetics.text}
+      </div>
+    );
   } else {
     return null;
   }
