853967fd7f48ab3d9c908ea580a6b64bfa76523b
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-12 16:32:03 +0100

    added a default keyword

--- src/Dictionaryform.js
+++ src/Dictionaryform.js
@@ -3,22 +3,26 @@ import axios from "axios";
 import Wordcalling from "./Dictionarycall";
 import "./App.css";
 
-export default function DictionaryForm() {
-  let [word, setword] = useState({});
+export default function DictionaryForm(props) {
+  let [word, setword] = useState(props.defaultKeyword);
   let [wordInfo, setwordInfo] = useState({});
   let apiKey = "3caeb6a7a0144t0bff6oc0e38972db67";
   let [data, setdata] = useState({});
+  let [loaded, setLoaded] = useState(false);
 
   function getphonetics(response) {
     console.log(response);
     setdata(response.data[0]);
   }
   function wordSearch(event) {
-    event.preventDefault();
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
     let apiUrltwo = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrltwo).then(getphonetics);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    wordSearch();
   }
 
   function handleResponse(response) {
@@ -29,24 +33,33 @@ export default function DictionaryForm() {
     console.log(event.target.value);
     setword(event.target.value);
   }
-  return (
-    <div className="definiton-body">
-      <div className="form">
-        <form onSubmit={wordSearch} className="">
-          <input
-            type="search"
-            onChange={handlingWord}
-            placeholder="Look for the word..."
-            className="mt-3 mb-3 p-2 border border-end-0 rounded rounded-end-0 searchBox"
-          />
-          <input
-            type="submit"
-            value="🔍"
-            className="border border-start-0 rounded rounded-start-0 p-2 mt-3 mb-3 submitButton"
-          />
-        </form>
+  function changeLoad() {
+    setLoaded(true);
+    wordSearch();
+  }
+  if (loaded) {
+    return (
+      <div className="definiton-body">
+        <div className="form">
+          <form onSubmit={handleSubmit} className="">
+            <input
+              type="search"
+              onChange={handlingWord}
+              placeholder="Look for the word..."
+              className="mt-3 mb-3 p-2 border border-end-0 rounded rounded-end-0 searchBox"
+            />
+            <input
+              type="submit"
+              value="🔍"
+              className="border border-start-0 rounded rounded-start-0 p-2 mt-3 mb-3 submitButton"
+            />
+          </form>
+        </div>
+        <Wordcalling need={wordInfo} data={data} />
       </div>
-      <Wordcalling need={wordInfo} data={data} />
-    </div>
-  );
+    );
+  } else {
+    changeLoad();
+    return "Loading...";
+  }
 }
