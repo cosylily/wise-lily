bf26bac58e089d14e5fd9145dca6163de623e739
cosylily <syafiqah.dayana1205@gmail.com>
2025-06-14 11:18:03 +0100

    deleted loaded
    webpage cannot be loaded for some reason

--- src/Dictionaryform.js
+++ src/Dictionaryform.js
@@ -8,7 +8,7 @@ export default function DictionaryForm(props) {
   let [wordInfo, setwordInfo] = useState({});
   let apiKey = "3caeb6a7a0144t0bff6oc0e38972db67";
   let [data, setdata] = useState({});
-  let [loaded, setLoaded] = useState(false);
+
   let [image, setImage] = useState({});
 
   function getphonetics(response) {
@@ -40,33 +40,25 @@ export default function DictionaryForm(props) {
     console.log(event.target.value);
     setword(event.target.value);
   }
-  function changeLoad() {
-    setLoaded(true);
-    wordSearch();
-  }
-  if (loaded) {
-    return (
-      <div className="definiton-body">
-        <div className="form">
-          <form onSubmit={handleSubmit} className="">
-            <input
-              type="search"
-              onChange={handlingWord}
-              placeholder="Look for the word..."
-              className="mt-3 mb-3 p-2 border border-end-0 rounded rounded-end-0 searchBox"
-            />
-            <input
-              type="submit"
-              value="🔍"
-              className="border border-start-0 rounded rounded-start-0 p-2 mt-3 mb-3 submitButton"
-            />
-          </form>
-        </div>
-        <Wordcalling need={wordInfo} data={data} photos={image} />
+
+  return (
+    <div className="definiton-body">
+      <div className="form">
+        <form onSubmit={handleSubmit} className="">
+          <input
+            type="search"
+            onChange={handlingWord}
+            placeholder="Look for the word..."
+            className="mt-3 mb-3 p-2 border border-end-0 rounded rounded-end-0 searchBox"
+          />
+          <input
+            type="submit"
+            value="🔍"
+            className="border border-start-0 rounded rounded-start-0 p-2 mt-3 mb-3 submitButton"
+          />
+        </form>
       </div>
-    );
-  } else {
-    changeLoad();
-    return "Loading...";
-  }
+      <Wordcalling need={wordInfo} data={data} photos={image} />
+    </div>
+  );
 }
